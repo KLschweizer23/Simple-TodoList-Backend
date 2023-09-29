fc55ff2af0d1d9d266080b2595bfbd129c65cbd5
Ken Lloyd Billones <Schweizer23.ss@gmail.com>
2023-09-29 22:50:59 +0800

    added update api request

--- server.js
+++ server.js
@@ -19,21 +19,55 @@ app.get('/api/get-all-data', (req, res) => {
 //body should contain "title"
 app.post('/api/add-data', async (req, res) => {
     let title = req.body.title
+
     if(!title){
-        res.status(400).json({ error: "Cannot add empty title." })
+        res.status(400).json({ error: "Title should not be empty!" })
         return
     }
-    const isAdded = addData(title)
-    if(!isAdded){
+    
+    if(!addData(title)){
         res.status(500).json({ error: "Something went wrong..." })
         return
     }
-    res.status(201).json({ message: "ToDo Item successfully added!" })
 
-    //Display data to the terminal
+    res.status(201).json({ message: "ToDo Item was successfully added!" })
+
+    displayData()
+})
+
+//Edit or Update data
+//Add the id in the parameter
+//body should contain "title"
+app.post('/api/update-data/:id', async (req, res) => {
+    const dataItem = data[req.params.id]
+    const newTitle = req.body.title
+
+    if(!dataItem){
+        res.status(404).json({ message: "Item not found!" })
+        return
+    }
+    if(!newTitle){
+        res.status(400).json({ error: "Title should not be empty!" })
+        return
+    }
+
+    dataItem.title = newTitle
+    if(!updateData(dataItem)){
+        res.status(500).json({ error: "Something went wrong..." })
+        return
+    }
+
+    res.status(201).json({ message: "ToDo Item was successfuly updated!" })
+
+    displayData()
+})
+
+//FUNCTIONS
+//Display data to the terminal
+function displayData(){
     console.log("data:")
     console.log(data)
-})
+}
 
 function addData(title){
     const dataToAdd = {}
@@ -43,6 +77,12 @@ function addData(title){
     dataToAdd.modifiedAt = getCurrentDateTime()
     data[dataToAdd.id] = dataToAdd
     return data[dataToAdd.id] != null
+}
+
+function updateData(newData){
+    newData.modifiedAt = getCurrentDateTime()
+    data[newData.id] = newData
+    return data[newData.id].title == newData.title
 }
 
 function getAvailableId(){
